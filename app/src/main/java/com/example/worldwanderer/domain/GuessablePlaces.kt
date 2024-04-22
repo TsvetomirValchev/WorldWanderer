4d391961288af3e5da15bbe8a9144a9005b58866
iraychev <ivoraychev1337@gmail.com>
2024-04-22 22:34:52 +0300

    new locations

--- app/src/main/java/com/example/worldwanderer/domain/GuessablePlaces.kt
+++ app/src/main/java/com/example/worldwanderer/domain/GuessablePlaces.kt
@@ -4,23 +4,25 @@ import com.google.android.gms.maps.model.LatLng
 
 object GuessablePlaces {
 
-    private val famousPlaceList = listOf(
+    private val placesList = listOf(
         LatLng(40.68871701100475, -74.04427434832975),             // Statue of Liberty, New York, USA
-        LatLng(48.858994, 2.293695),                               // Eiffel Tower, Paris, France
-        LatLng(51.500965, -0.124230),                              // Big Ben, London, UK
+        LatLng(48.85965995677045, 2.2946408998280288),             // Eiffel Tower, Paris, France
+        LatLng(51.50095483791037, -0.12571488204791229),           // Big Ben, London, UK
         LatLng(43.722874, 10.395610),                              // Leaning Tower of Pisa, Pisa, Italy
-        LatLng(41.890210, 12.490246),                              // Colosseum, Rome, Italy
+        LatLng(41.89107157930109, 12.490863223085935),             // Colosseum, Rome, Italy
+        LatLng(19.436304177267342, -99.15561412340433),            // Monument to the Revolution, Mexico City
         LatLng(34.134187, -118.321615),                            // Hollywood Sign, Los Angeles, USA
-        LatLng(51.502912, -0.117386),                              // Buckingham Palace, London, UK
-        LatLng(41.402655, 2.174312),                               // Sagrada Família, Barcelona, Spain
+        LatLng(41.40352175689611, 2.1732084157399107),             // Sagrada Família, Barcelona, Spain
         LatLng(-33.856144, 151.215155),                            // Sydney Opera House, Sydney, Australia
-        LatLng(55.752563, 37.623820),                              // Red Square, Moscow, Russia
-        LatLng(48.874060, 2.294341),                               // Louvre Museum, Paris, France
+        LatLng(55.75271518994579, 37.62367616516008),              // Red Square, Moscow, Russia
+        LatLng(48.874170372979364, 2.2945712915352066),            // Arc de Triomphe, Paris, France
         LatLng(51.178936, -1.826430),                              // Stonehenge, Wiltshire, UK
         LatLng(27.174229, 78.042182),                              // Taj Mahal, Agra, India
         LatLng(29.975264, 31.138207),                              // Pyramids of Giza, Cairo, Egypt
-        LatLng(48.860413, 2.337558),                               // Arc de Triomphe, Paris, France
+        LatLng(48.86083144497461, 2.3363173441857827),             // Louvre Museum, Paris, France
+        LatLng(24.470431464021484, 54.60806308339859),             // Yas Marina Circuit, Abu Dhabi
         LatLng(51.501284, -0.141536),                              // London Eye, London, UK
+        LatLng(1.298790724645449, 103.85758580386735),             // Singapore
         LatLng(-22.951985, -43.209790),                            // Christ the Redeemer, Rio de Janeiro, Brazil
         LatLng(25.199022, 55.273389),                              // Burj Khalifa, Dubai, UAE
         LatLng(3.156877, 101.712642),                              // Petronas Twin Towers, Kuala Lumpur, Malaysia
@@ -31,7 +33,9 @@ object GuessablePlaces {
         LatLng(36.1010805, -115.15806),                            // Las Vegas Strip, Las Vegas, USA
         LatLng(35.689487, 139.691711),                             // Tokyo Tower, Tokyo, Japan
         LatLng(40.416775, -3.703790),                              // Plaza Mayor, Madrid, Spain
-        LatLng(-25.670997, 28.523736),                             // Union Buildings, Pretoria, South Africa
+        LatLng(-25.741831863490447, 28.211656043124265),           // Nelson Mandela statue, Pretoria, South Africa
+        LatLng(41.09372990593703, 29.064028099518218),             // Bosphorus, Istanbul, Turkey
+        LatLng(40.65247678071708, 22.939820181134024),             // Solun, Greece
         LatLng(37.811053353770156, -122.47560479341807),           // Golden Gate Bridge, San Francisco, USA
         LatLng(35.45490851927871, 139.63130999315223),             // Yokohama Landmark Tower, Yokohama, Japan
         LatLng(59.94032910795738, 30.3281407512523),               // Church of the Savior on Blood, St. Petersburg, Russia
@@ -63,9 +67,9 @@ object GuessablePlaces {
     fun getFamousPlaceList():Set<LatLng>
     {
         val list = mutableSetOf<LatLng>()
-        while (list.size< famousPlaceList.size)
+        while (list.size< placesList.size)
         {
-            list.add(famousPlaceList.random())
+            list.add(placesList.random())
         }
         return list
     }
