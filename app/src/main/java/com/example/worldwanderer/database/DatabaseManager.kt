50d39cc574a807c30e75774d7ee0c50727d2c25d
Tsvetomir Valchev <tsvetomir.valchev2@gmail.com>
2024-04-24 19:27:23 +0300

    add comments that explain eache method

--- app/src/main/java/com/example/worldwanderer/database/DatabaseManager.kt
+++ app/src/main/java/com/example/worldwanderer/database/DatabaseManager.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import com.example.worldwanderer.activity.GuessPlaceActivity
+import com.example.worldwanderer.activity.MainActivity
 import com.example.worldwanderer.adapter.LeaderboardEntryAdapter
 import com.example.worldwanderer.databinding.ActivityLeaderboardBinding
 import com.example.worldwanderer.domain.LeaderboardEntry
@@ -16,50 +16,83 @@ import com.google.firebase.database.ValueEventListener
 
 class DatabaseManager(private val context: Context) {
 
-    private val databaseReference: DatabaseReference = FirebaseDatabase.getInstance("https://world-wanderer-e19f2-default-rtdb.europe-west1.firebasedatabase.app/").getReference("highscores")
+    // Reference to the Firebase database
+    private val databaseReference: DatabaseReference =
+        FirebaseDatabase.getInstance("https://world-wanderer-e19f2-default-rtdb.europe-west1.firebasedatabase.app/").getReference("highscores")
+
+    // Adapter class for the leaderboard entries
     lateinit var leaderboardAdapter: LeaderboardEntryAdapter
 
-
+    // Fetches leaderboard entries from the database
     fun fetchDataFromDatabase(leaderboardEntries: MutableList<LeaderboardEntry>, binding: ActivityLeaderboardBinding) {
         databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Clear the existing list of leaderboard entries
                 leaderboardEntries.clear()
+                // Iterate through each child node in the database snapshot
                 for (snapshot in dataSnapshot.children) {
+                    // Retrieve the total number of entries in the leaderboard
                     val totalEntries = dataSnapshot.childrenCount
+                    // Retrieve the encoded email and decode it
                     val encodedEmail = snapshot.key ?: ""
+                    // Decode the email retrieved from the database, so that it looks like an actual email in the view
                     val email = decodeEmail(encodedEmail)
+                    // Retrieve the score associated with the email
                     val score = (snapshot.value as? Long)?.toInt() ?: 0
-                    "Total highscores recorded : $totalEntries".also { binding.totalScores.text = it }
+                    // Update the total highscores recorded in the UI
+                    "Total highscores recorded : $totalEntries".also {
+                        binding.totalScores.text = it
+                    }
+                    // Add the leaderboard entry to the list
                     leaderboardEntries.add(LeaderboardEntry(email, score))
+                    // Updates the UI with the new leaderboard data
+                    updateLeaderboardUI()
                 }
-                leaderboardEntries.sortByDescending { it.score }
-                updateLeaderboardUI()
             }
-
+            // Handle database error by starting MainActivity
             override fun onCancelled(error: DatabaseError) {
-                context.startActivity(Intent(context, GuessPlaceActivity::class.java))
+                context.startActivity(Intent(context, MainActivity::class.java))
                 (context as AppCompatActivity).finish()
             }
         })
     }
 
+    // Saves user score to the database if it's higher than the current score
     fun saveUserScoreToDatabase(userEmail: String, score: Int) {
-        databaseReference.child(userEmail).setValue(score)
+        // Retrieve the current score from the database
+        databaseReference.child(userEmail)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val currentScore = dataSnapshot.getValue(Int::class.java) ?: 0
+                    // Check if the new score is higher than the current score
+                    if (score > currentScore) {
+                        // Update the score in the database only if the new score is higher
+                        databaseReference.child(userEmail).setValue(score)
+                    }
+                }
+                // Handle database error by starting MainActivity
+                override fun onCancelled(error: DatabaseError) {
+                    context.startActivity(Intent(context, MainActivity::class.java))
+                    (context as AppCompatActivity).finish()
+                }
+            })
     }
 
+    // Encodes an email address for use as a database key
     fun encodeEmail(email: String?): String {
         return email!!.replace('.', ',')
     }
 
+    // Decodes an encoded email address
     private fun decodeEmail(encodedEmail: String): String {
         return encodedEmail.replace(',', '.')
     }
 
+    // Updates the UI with the new leaderboard data
     @SuppressLint("NotifyDataSetChanged")
     private fun updateLeaderboardUI() {
         leaderboardAdapter.notifyDataSetChanged()
     }
-
 
 
 }
